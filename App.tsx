fa28db9444a53cdf12344598d33e134dc8bb5cf0
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-04-21 10:40:53 -0300

    add user identification

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AppLoading from 'expo-app-loading'
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
 
-import { Welcome } from './src/pages/Welcome'
+import { UserIdentification } from './src/pages/UserIdentification'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,6 +13,6 @@ export default function App() {
   if (!fontsLoaded) return <AppLoading />
 
   return (
-    <Welcome />
+    <UserIdentification />
   )
 }
